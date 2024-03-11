fa3d0c7ce8ea68a732d64b341b07cb5ba2bfae9f
mkvaghani <alinashevchenko.me@gmail.com>
2024-03-11 09:10:52 +0530

    Fix: Cart drawer css

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * @package Base
  */
 
-define( 'AVANAM_VERSION', '1.3.7' );
+define( 'AVANAM_VERSION', '1.3.8' );
 define( 'AVANAM_MINIMUM_WP_VERSION', '5.4' );
 define( 'AVANAM_MINIMUM_PHP_VERSION', '7.2' );
 

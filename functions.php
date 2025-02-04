56e97de3d74e008b84d0661d414d520cc0e25ed6
alinashevchenkome <alinashevchenko.me@gmail.com>
2025-02-04 17:55:31 +0530

    Fix payment button

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * @package Base
  */
 
-define( 'AVANAM_VERSION', '1.5.0' );
+define( 'AVANAM_VERSION', '1.5.1' );
 define( 'AVANAM_MINIMUM_WP_VERSION', '6.0' );
 define( 'AVANAM_MINIMUM_PHP_VERSION', '7.4' );
 
